49761eb793f79803cf4d773c4731243c092edf5f
mohit0527 <38519628+mohit0527@users.noreply.github.com>
2021-04-16 14:20:04 +0530

    Create function.h

---
+++ 3_Implementation/inc/function.h
@@ -0,0 +1,12 @@
+/**
+ * @brief Header file containing all the necessary functions
+ * 
+ * @param k 
+ * @param g 
+ * @param z 
+ */
+void p1ch(int k,char** g,int z);
+void p2ch(int k,char** g,int z);
+void grid(char** g,int z);
+int checkp1(char** g,int z);
+int checkp2(char** g,int z);
